38f99530d1061ac6bf83d966234ae9afa13be81d
Sifaw-Bouy <sifaw117@outlook.com>
2021-11-06 18:50:52 -0400

    Menu added
    >added main menu and help menu
    >add timer

---
+++ Virtual Reality Project/Assets/Code/Mainmenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void StartGame (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
---
+++ Virtual Reality Project/Assets/Code/Timercounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timercounter : MonoBehaviour
+{
+    public float countTval = 110;
+    public Text countTimer;
+    void Update()
+    {
+        if(countTval>0){
+               countTval -= Time.deltaTime;   
+        }else{
+            countTval = 0; 
+        }
+        countdown(countTval);
+    }
+    public void countdown( float time){
+        float min;
+        float sec;
+        if(time < 0){
+            time = 0;
+        }
+        min = Mathf.FloorToInt(time / 60);//get the mintues
+        sec = Mathf.FloorToInt(time % 60);//get the seconds
+        countTimer.text = string.Format("{0:00}:{1:00}", min,sec);
+    }
+}
